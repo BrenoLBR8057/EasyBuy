17c337a93fb9c1954dd017fefc4a7d080e13c3e7
ivaldo.281617 <juniorbalancista@gmail.com>
2020-11-18 18:10:39 -0300

    18/11/2020

--- app/src/main/java/com/example/easybuy/ProductsList.java
+++ app/src/main/java/com/example/easybuy/ProductsList.java
@@ -2,13 +2,32 @@ package com.example.easybuy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ProductsList extends AppCompatActivity {
+
+    private Button btnSaveList;
+    public static final String PRODUCTS_LIST = "PRODUCT_SAVE";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_products_list);
+    }
+    private void clickButtons(){
+        btnSaveList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ProductsList.this,ProductsList.class);
+                intent.putExtra(PRODUCTS_LIST , productList);
+                setResult(Activity.RESULT_OK, intent);
+            }
+        });
+
     }
 }
